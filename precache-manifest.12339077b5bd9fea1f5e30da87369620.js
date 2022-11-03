17dff30ba8b451fd8eda0ec4946abcd37f2a7c32
atn68 <anhtuan29592@gmail.com>
2022-11-03 14:25:54 +0700

    update

---
+++ precache-manifest.12339077b5bd9fea1f5e30da87369620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b0b6519bdbc1a6ec2e0c475218fbb",
+    "url": "/atn68/index.html"
+  },
+  {
+    "revision": "103719c9607a5183458c",
+    "url": "/atn68/static/css/2.6da7ffee.chunk.css"
+  },
+  {
+    "revision": "7984b65dc8961114fdc3",
+    "url": "/atn68/static/css/main.6771c0cf.chunk.css"
+  },
+  {
+    "revision": "103719c9607a5183458c",
+    "url": "/atn68/static/js/2.4ad563f1.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/atn68/static/js/2.4ad563f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7984b65dc8961114fdc3",
+    "url": "/atn68/static/js/main.ab4cff6d.chunk.js"
+  },
+  {
+    "revision": "790494a92b9031f69326",
+    "url": "/atn68/static/js/runtime-main.440d3bc4.js"
+  }
+]);
